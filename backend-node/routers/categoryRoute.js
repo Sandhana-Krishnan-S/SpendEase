8d7e2c9a8ea0e4968f4d79297605fb20e52b5fc6
Sandhana-Krishnan-S <sandhanakrishnansakthivel@gmail.com>
2025-04-28 00:24:29 +0530

    delete category - soft done

--- backend-node/routers/categoryRoute.js
+++ backend-node/routers/categoryRoute.js
@@ -1,8 +1,11 @@
 const addCategoryController = require('../controller/categoryController/addCategoryController');
+const deleteCategoryController = require('../controller/categoryController/deleteCategoryController');
 const getCategoryController = require('../controller/categoryController/getCategoryController');
 const categoryRoute = require('express').Router();
 
 categoryRoute.post('/' , addCategoryController);
 categoryRoute.get('/' , getCategoryController);
+//update
+categoryRoute.delete('/' , deleteCategoryController);
 
 module.exports = categoryRoute;
---
+++ backend-node/service/categoryService/deleteCategoryService.js
@@ -0,0 +1,25 @@
+const categoryModel = require("../../model/categoryModel");
+
+const deleteCategoryService = async (userId, categoryId) => {
+    try {
+        const category = await categoryModel.findByIdWithUser(categoryId , userId);
+        if(!category) {
+            return {
+                status : false,
+                data : null,
+                error : "Category not found"
+            }
+        }
+        category.isDeleted = true;
+        const updated = await category.save();
+        return {
+            status : true,
+            data : updated,
+            error : null
+        };
+    } catch (error) {
+        throw(error);
+    }
+}
+
+module.exports = deleteCategoryService;
